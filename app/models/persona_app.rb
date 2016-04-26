4f6f64c4cdc3147d75516a19fc9cee76cedb82f2
BK Bolisetty <bk@blitzkraft.me>
2016-04-25 20:55:13 -0500

    Hierarchy: User > App > Archive.

--- app/models/persona_app.rb
+++ app/models/persona_app.rb
@@ -1,3 +1,5 @@
 class PersonaApp < ActiveRecord::Base
 	belongs_to :user
+	has_one :app_archive
+	has_many :screenshots
 end
--- app/models/screenshot.rb
+++ app/models/screenshot.rb
@@ -1,4 +1,4 @@
 class Screenshot < ActiveRecord::Base
-	belongs_to :persona_app
 	belongs_to :user
+	belongs_to :persona_app
 end
